9e6b9524101890616522a29d3d4134b23d799b4a
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2022-10-29 21:03:26 -0700

    10/29 completed code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const hourEl = document.querySelector('.hour')
+sconst hourEl = document.querySelector('.hour')
 const minuteEl = document.querySelector('.minute')
-const seconEl = document.querySelector('.second')
+const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const date = document.querySelector('.date')
 const toggle = document.querySelector('.toggle')
@@ -24,11 +24,20 @@ function setTime(){
     const month = time.getMonth();
     const day = time.getDay();
     const hours = time.getHours();
-    const hoursForClock = hours % 24
+    const hoursForClock = hours % 12
     const minutes = time.getMinutes();
     const seconds = time.getSeconds();
 
-    hourEl.style.transform = `translate(-50%, -100%) rotate()`
+    hourEl.style.transform = `translate(-50%, -100%) rotate(${scale(hoursForClock, 0, 11, 0, 360)}deg)`
+    minuteEl.style.transform = `translate(-50%, -100%) rotate(${scale(minutes, 0, 59, 0, 360)}deg)`
+    secondEl.style.transform = `translate(-50%, -100%) rotate(${scale(seconds, 0, 59, 0, 360)}deg)`
+
+    timeEl.innerHTML = `${hours}:${minutes < 10 ? `0${minutes}`: minutes}`
+    date.innerHTML = `${days[day]}, ${months[month]}<span class"circle>${date}</span>`
+}
+const scale = (number, inMin, inMax, outMin, outMax) => {
+    return (number - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
 setTime()
+setInterval(setTime, 1000)
